13288763fd6300e4f2845fcdbf197a86031afd88
daniel08022 <74110809+daniel08022@users.noreply.github.com>
2021-03-05 18:00:06 -0500

    actualizacion php

--- enviar.php
+++ enviar.php
@@ -1,11 +1,25 @@
+      
 <?php
-$destino = "danielestupinan0802@gmail.com";
-    $nombre = $_POST["nombre"];
-    $email = $_POST["email"];
-    $numero = $_POST["numero"];
-    $mensaje = $_POST["mensaje"];
-    $contenido = "Nombre: " . $nombre . "\nCorreo: " . $email . "\nNúmero: " . $numero . "\nAsunto: " . $mensaje;
-    mail($destino, "Contacto", $contenido);
-    header("Location:index.html");
-//Esto es opcional, aqui pueden colocar un mensaje de "enviado correctamente" o redireccionarlo a algun lugar
+$name = $_POST['nombre'];
+$mail = $_POST['email'];
+$phone = $_POST['numero'];
+$message = $_POST['mensaje'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'danielestupinan0802@gmail.com';
+$asunto = 'Mensaje de PORTAFOLIO';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
 ?>
