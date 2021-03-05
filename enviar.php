e45937bcb296c647dec9c2c5f1b0aa914c251562
daniel08022 <74110809+daniel08022@users.noreply.github.com>
2021-03-05 17:26:25 -0500

    Siu

--- enviar.php
+++ enviar.php
@@ -1,12 +1,11 @@
 <?php
 $destino = "danielestupinan0802@gmail.com";
     $nombre = $_POST["nombre"];
-    $apellido = $_POST["apellido"];
     $email = $_POST["email"];
     $numero = $_POST["numero"];
     $mensaje = $_POST["mensaje"];
-    $contenido = "Nombre: " . $nombre . "\nApellido: " . $apellido . "\nCorreo: " . $email . "\nNúmero: " . $numero . "\nAsunto: " . $mensaje;
+    $contenido = "Nombre: " . $nombre . "\nCorreo: " . $email . "\nNúmero: " . $numero . "\nAsunto: " . $mensaje;
     mail($destino, "Contacto", $contenido);
-    header("Location: A_Donde_Lo_Quieras_Redireccionar");
+    header("Location:index.html");
 //Esto es opcional, aqui pueden colocar un mensaje de "enviado correctamente" o redireccionarlo a algun lugar
 ?>
