e9459bb6737b82cad7189f54f39ecababeab95fd
daniel08022 <74110809+DanielE0802@users.noreply.github.com>
2021-03-16 17:13:36 -0500

    Update nav

--- js/app.js
+++ js/app.js
@@ -1,41 +1,21 @@
-
-const seccionesPagina = new fullpage('#fullpage',{
+const seccionesPagina = new fullpage('#fullpage', {
 	navigation: true,
 	navigationPosition: 'right',
-	anchors: ['Inicio', 'Acerca', 'Proyectos','Contacto'],
-	navigationTooltips: ['Inicio', 'Acerca', 'Proyectos','Contacto'],
+	anchors: ['Inicio', 'Acerca', 'Proyectos', 'Contacto'],
+	navigationTooltips: ['Inicio', 'Acerca', 'Proyectos', 'Contacto'],
 	scrollingSpeed: 800,
 	easing: 'easeInOutCubic',
-	menu: '#miMenu', 
+	menu: '#miMenu',
 	showActiveTooltip: true,
+	onLeave: function (origin, destination, direction) {
+		console.log(direction)
+		if(origin.anchor == 'Inicio'){
+			nav.classList.add("nuevoBar");
+		}
+
+		else if( origin.anchor == 'Acerca' & direction == 'up'){
+			nav.classList.remove("nuevoBar");
+		}
+
+	}
 });
-
-
-setInterval(() => {
-	var inicio= document.getElementById("inicio").getAttribute("class");
-	var mi= document.getElementById("mi").getAttribute("class")
-	if(inicio == "active"){
-		nav.classList.remove("nuevoBar");
-	}
-	else{
-		nav.classList.add("nuevoBar");
-	}
-}, 300); 
-
-
-
-
-
-// window.addEventListener("", function(e){
-// 	cambió=true;
-// 	if(cambio=true){
-// 		nav.classList.toggle("nuevoBar");
-// 		alert("encima")
-// 	}
-	
-// 	console.log(cambió)
-// },false)
-
-// console.log(cambió)
-
-
